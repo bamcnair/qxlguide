d7fcb788c69a4d81760cf4fb344157f6853e5104
bamcnair <bamcnair@gmail.com>
2016-10-17 18:06:54 -0400

    v0.1.73

--- src/app.js
+++ src/app.js
@@ -367,7 +367,7 @@ function event_eventbrite(location, senduser){
 						  }
 						}
 					  };	console.log("This is the message array but Right before the Return " + JSON.stringify(messageData));
-					  sendFBMessage(sender,JSON.stringify(messageData));
+					  sendFBMessage(senduser,JSON.stringify(messageData));
 					  
 			 });	 
 }
