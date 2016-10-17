d49a800b979ff5d169aa3df440eb9b12330de486
bamcnair <bamcnair@gmail.com>
2016-10-17 12:17:54 -0400

    v0.1.22

--- src/app.js
+++ src/app.js
@@ -320,7 +320,7 @@ function event_eventbrite(locate, senduser){
 				//var eventbapi = JSON.parse(ebody);
 				//var eventbrite = eventbapi.events;
 				var eventbrite = ebody.events;
-				var numofevents = eventbrite.length;
+				//var numofevents = eventbrite.length;
 				//This code checks if events are available from eventbrite.  If num of events is zero, there's nothing to show
 				
 					if (ebody.pagination){
