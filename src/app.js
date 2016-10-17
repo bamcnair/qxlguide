630e273a47364ea7cb205c108e3ae5eadd28f639
bamcnair <bamcnair@gmail.com>
2016-10-17 12:07:34 -0400

    v0.1.19

--- src/app.js
+++ src/app.js
@@ -317,12 +317,12 @@ function event_eventbrite(locate, senduser){
 
 	var ebody = event_eventbrite_apicall(locate);
 
-				var eventbapi = JSON.parse(ebody);
-				var eventbrite = eventbapi.events;
-				var numofevents = eventbrite.length;
+				//var eventbapi = JSON.parse(ebody);
+				//var eventbrite = eventbapi.events;
+				//var numofevents = eventbrite.length;
 				//This code checks if events are available from eventbrite.  If num of events is zero, there's nothing to show
 				
-					if (ebody){
+					if (ebody.events.length){
 					return ("topvalue2");
 					}
  	/*           
