f90f9df70825877bb48465e6e381e2dbc02a88fb
bamcnair <bamcnair@gmail.com>
2016-10-17 23:00:37 -0400

    v0.1.89

--- src/app.js
+++ src/app.js
@@ -405,7 +405,7 @@ function event_meetup(mcity, mzipcode, senduser){
 				var messageDatam = [];
 
 		request({
-		  url: 'https://api.meetup.com/2/open_events?key=7b196b2b6510335c99242643b2a53&sign=true&topic=beer&zip='+mzipcode+'&radius=30&city='+mcity,
+		  url: 'https://api.meetup.com/2/open_events?key=7b196b2b6510335c99242643b2a53&sign=true&topic=weed,cannabis,marijuana&zip='+mzipcode+'&radius=30&city='+mcity,
 			method: 'GET'
 		},(error, response, body) => {
 			 if (!error && (response.code == "bad_request" || response.code == "invalid_param")) {
