ee44eec7f5713f0c097f3532b974c7210a3695fa
bamcnair <bamcnair@gmail.com>
2016-11-05 12:21:57 -0400

    v0.2.85

--- src/app.js
+++ src/app.js
@@ -424,8 +424,8 @@ function event_eventbrite(location, senduser){
 						var edate = bb1.events[ie].start.local;
 						var elink = bb1.events[ie].url;
 
-							if(!bb1.events[ie].logo.url){
-								bb1.events[ie].logo.url = "https://en.wikipedia.org/wiki/Smiley#/media/File:Smiley.svg";
+							if(!eimage){
+								bb1.events[ie].logo.url = "https://lh3.googleusercontent.com/4bh7BjYBV7S5fHX6_slB_tUfzl9-oTqUdWotoG5QMMLjM6z3BQe73I35aYfCB9J70tDPuxpbJ0Fnsk996nSVIeIWTSLLSJO6KbK7_NZrY4-KGAXkdGJmt1PINHZfOy5b49FzMfreYpwYSa_Td8bys50icZFaFTQVU7f1m3WHh-8PgxscQXDE-dNtvPHtcZQmdG9sAyqZ9fEPCQx6NyvcskkETvy_K8-xqMKhEseXbt5a5cvsTtNkTDLUXco6Lqp1RId5WUQnLpIQ_h1IiJDhRD4jtmP-I9L5P8GB0kWUmjCwQzqUu8UozFjArsGlPGfoL_OgFBl6yyImoEs5rSA6svqjzieAXnKfKu7CBX1mmw_MDjMHcb16UEAKG0foZ69Vk3mNpvl_Kf2Z-l93zzPjNrf3_re35HZ1_YRyPI1snWuc_mW4_kUml0rG8qoaRgx7tDiSlZTOGyyRggNTf6cRoZofo2n6Z43Sirsoqbdo6BG8w4MIGOHwE0dJWL3AQxgfO0QZjQzz_HgVRJ_iHx9X23hh0APZgd-ZBDVXu1oyYJRJR030EXGnjvMQDR-QDBHDfG95Xnta3vutS2Blib-y9CPpF66YzvJQdGvGNAAMiVORoR4=w1660-h768-no";
 								//CHANGE THIS TO myTHCGuide logo once we choose one!
 							}
 						elementsar.push({
@@ -523,7 +523,7 @@ function event_meetup(mcity, mzipcode, senduser){
 								title: mtitle,
 								//subtitle: mdate,
 								item_url: mlink,               
-								image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/JfTabon%2C_San_Isidro%2C_Nueva_Ecijalands0088fvf_04.JPG/640px-JfTabon%2C_San_Isidro%2C_Nueva_Ecijalands0088fvf_04.JPG', //mimage,
+								image_url: 'https://lh3.googleusercontent.com/4bh7BjYBV7S5fHX6_slB_tUfzl9-oTqUdWotoG5QMMLjM6z3BQe73I35aYfCB9J70tDPuxpbJ0Fnsk996nSVIeIWTSLLSJO6KbK7_NZrY4-KGAXkdGJmt1PINHZfOy5b49FzMfreYpwYSa_Td8bys50icZFaFTQVU7f1m3WHh-8PgxscQXDE-dNtvPHtcZQmdG9sAyqZ9fEPCQx6NyvcskkETvy_K8-xqMKhEseXbt5a5cvsTtNkTDLUXco6Lqp1RId5WUQnLpIQ_h1IiJDhRD4jtmP-I9L5P8GB0kWUmjCwQzqUu8UozFjArsGlPGfoL_OgFBl6yyImoEs5rSA6svqjzieAXnKfKu7CBX1mmw_MDjMHcb16UEAKG0foZ69Vk3mNpvl_Kf2Z-l93zzPjNrf3_re35HZ1_YRyPI1snWuc_mW4_kUml0rG8qoaRgx7tDiSlZTOGyyRggNTf6cRoZofo2n6Z43Sirsoqbdo6BG8w4MIGOHwE0dJWL3AQxgfO0QZjQzz_HgVRJ_iHx9X23hh0APZgd-ZBDVXu1oyYJRJR030EXGnjvMQDR-QDBHDfG95Xnta3vutS2Blib-y9CPpF66YzvJQdGvGNAAMiVORoR4=w1660-h768-no', //mimage,
 								buttons: [{
 								  type: "web_url",
 								  url: mlink,
@@ -591,6 +591,7 @@ function specific_strain(cr_strain,cr_senduser){
 					*/
                    // sendFBMessage(sender, {text: 'Unfortunately we did not find any strains'});					
 					//return;
+					console.log("We didn't find any strains for that name " + numofstrains);
 					}
 				else if(numofstrains == 1){
 				
