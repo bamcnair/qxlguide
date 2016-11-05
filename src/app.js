3f1bba7b7e5b33634141a626d721c44f009218c6
bamcnair <bamcnair@gmail.com>
2016-11-05 11:39:55 -0400

    v0.2.73

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function processEvent(event) {
 				let responseData = response.result.fulfillment.messages;
                 let action = response.result.action;
 				
-				if (response.result.fulfillment.messages[0].payload){
+				if (typeof response.result.fulfillment.messages[0].payload === null  || response.result.fulfillment.messages[0].payload === undefined || response.result.fulfillment.messages[0].payload.length <= 0){
 				console.log("Payload variable is undefined");
 				//responseData = response.result.fulfillment.messages[0].payload;
 				}
