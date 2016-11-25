13f1c074ef474bd7cc23bfd0ae6eaea5f662c319
bamcnair <bamcnair@gmail.com>
2016-11-25 15:13:13 -0500

    v0.3.27

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 
-   
+
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
